0b1dfad4c075f98568555e28253afe4377f3cd9a
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-07-23 23:31:23 +0100

    List filtering, Cover Saving, Serial ID identification, On Demand Description
    Conflicts:
    	Source/ui_android/java/com/virtualapplications/play/GameInfo.java
    	Source/ui_android/java/com/virtualapplications/play/GamesDbAPI.java
    	Source/ui_android/java/com/virtualapplications/play/MainActivity.java

--- Source/ui_android/java/com/virtualapplications/play/MainActivity.java
+++ Source/ui_android/java/com/virtualapplications/play/MainActivity.java
@@ -11,12 +11,7 @@ import android.view.*;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.*;
-import android.widget.AdapterView.*;
-import android.widget.ImageView.ScaleType;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -25,9 +20,12 @@ import android.support.v4.widget.DrawerLayout;
 import java.io.*;
 import java.text.*;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.zip.*;
 import org.apache.commons.lang3.StringUtils;
 import com.android.util.FileUtils;
+
+import static org.apache.commons.lang3.text.WordUtils.capitalizeFully;
 
 public class MainActivity extends Activity 
 {	
@@ -35,14 +33,13 @@ public class MainActivity extends Activity
 	
 	private SharedPreferences _preferences;
 	private TableLayout gameListing;
-	static Activity mActivity;
+	private static Activity mActivity;
 	private boolean isConfigured = false;
 	private int numColumn = 0;
 	private DrawerLayout mDrawerLayout;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private float localScale;
 	private int currentOrientation;
-	private GameInfo gameInfo;
 	
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) 
@@ -119,12 +116,21 @@ public class MainActivity extends Activity
 		{
 			NativeInterop.createVirtualMachine();
 		}
-		
-		gameInfo = new GameInfo(MainActivity.this);
-		gameInfo.getDatabase();
-		
+
+		initialDbSetup();
 		prepareFileListView();
 	}
+
+	private void initialDbSetup() {
+		try {
+			new SetupDB().execute(this).get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	private static long getBuildDate(Context context) 
 	{
@@ -193,6 +199,14 @@ public class MainActivity extends Activity
 		
 	}
 	
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu)
+//	{
+//		MenuInflater inflater = getMenuInflater();
+//		inflater.inflate(R.layout.main_menu, menu);
+//		return super.onCreateOptionsMenu(menu);
+//	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) 
 	{
@@ -210,6 +224,12 @@ public class MainActivity extends Activity
 				mDrawerLayout.openDrawer(Gravity.LEFT);
 			}
 			return true;
+//		case R.id.main_menu_settings:
+//			displaySettingsActivity();
+//			return true;
+//		case R.id.main_menu_about:
+//			displayAboutDialog();
+//			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
@@ -323,10 +343,19 @@ public class MainActivity extends Activity
 			
 			FileUtils fileUtils = new FileUtils();
 			Collection<File> files = fileUtils.listFiles(storage, filter, -1);
+			Iterator<File> imagesit = files.iterator();
+			while (imagesit.hasNext()) {
+				File images = imagesit.next();
+				if (!IsLoadableExecutableFileName(images.getName())){
+					if (NativeInterop.GetDiskId(images.toString()) == null){
+						imagesit.remove();
+					}
+				}
+			}
 			return (List<File>) files;
 		}
 		
-		private View createListItem(final File game, final int index) {
+		private View createListItem(final File game) {
 			
 			if (!isConfigured) {
 				
@@ -350,40 +379,37 @@ public class MainActivity extends Activity
 			
 			final View childview = MainActivity.this.getLayoutInflater().inflate(
 				R.layout.game_list_item, null, false);
+
+			final getGameDetails GG = new getGameDetails(game, mActivity);
+			GG.setViewParent(childview);
+
+			childview.findViewById(R.id.childview).setOnLongClickListener(new OnLongClickListener() {
+				public boolean onLongClick(View view) {
+					final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+					builder.setCancelable(true);
+					builder.setTitle(capitalizeFully(GG.GameInfo.getName()));
+					builder.setIcon(new BitmapDrawable(GG.GameInfo.getBackCover()));
+					builder.setMessage(GG.GameInfo.getDescription(mActivity));
+					builder.setPositiveButton("Close",
+											  new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+							return;
+						}
+					});
+					builder.setPositiveButton("Launch",
+											  new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+							launchDisk(game);
+							return;
+						}
+					});
+					builder.create().show();
+					return true;
+				}
+			});
 			
-			((TextView) childview.findViewById(R.id.game_text)).setText(game.getName());
-			
-			final String[] gameStats = gameInfo.getGameInfo(game, childview);
-			
-			if (gameStats != null) {
-				childview.findViewById(R.id.childview).setOnLongClickListener(new OnLongClickListener() {
-					public boolean onLongClick(View view) {
-						final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-						builder.setCancelable(true);
-						builder.setTitle(gameStats[1]);
-						builder.setMessage(gameStats[2]);
-						builder.setNegativeButton("Close",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int which) {
-								dialog.dismiss();
-								return;
-							}
-						});
-						builder.setPositiveButton("Launch",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int which) {
-								dialog.dismiss();
-								launchDisk(game);
-								return;
-							}
-						});
-						builder.create().show();
-						return true;
-					}
-				});
-				gameInfo.getImage(gameStats[0], childview);
-				((TextView) childview.findViewById(R.id.game_text)).setVisibility(View.GONE);
-			}
 			
 			childview.findViewById(R.id.childview).setOnClickListener(new OnClickListener() {
 				public void onClick(View view) {
@@ -391,6 +417,7 @@ public class MainActivity extends Activity
 					return;
 				}
 			});
+			GG.execute(game.getName());
 			return childview;
 		}
 		
@@ -445,7 +472,7 @@ public class MainActivity extends Activity
 
 					for (int i = 0; i < images.size(); i++)
 					{
-						game_row.addView(createListItem(images.get(i), i));
+						game_row.addView(createListItem(images.get(i)));
 						gameListing.addView(game_row, params);
 						game_row = new TableRow(MainActivity.this);
 						game_row.setPadding(0, 0, 0, pad);
@@ -467,7 +494,7 @@ public class MainActivity extends Activity
 							game_row.setGravity(Gravity.CENTER);
 							game_row.setPadding(0, 0, 0, pad);
 						}
-						game_row.addView(createListItem(images.get(i), i));
+						game_row.addView(createListItem(images.get(i)));
 						column ++;
 					}
 					if (column != 0) {
@@ -479,10 +506,6 @@ public class MainActivity extends Activity
 				// Display warning that no disks exist
 			}
 		}
-	}
-	
-	public static void launchGame(File game) {
-		((MainActivity) mActivity).launchDisk(game);
 	}
 	
 	private void launchDisk (File game) {
@@ -521,10 +544,6 @@ public class MainActivity extends Activity
 
 	private void prepareFileListView()
 	{
-		if (gameInfo == null) {
-			gameInfo = new GameInfo(MainActivity.this);
-		}
-		
 		gameListing = (TableLayout) findViewById(R.id.game_grid);
 		if (gameListing != null) {
 			gameListing.removeAllViews();
